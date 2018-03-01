a32a05470f7af79cde68e758b26dfa27f5eeb680
Matt Mielke <matt.mielke@digikey.com>
2018-03-01 11:48:41 -0600

    Fixing bug in crc_8 function

--- atmosphere/sht31_element_library/driver/common/objects/sht31_common.c
+++ atmosphere/sht31_element_library/driver/common/objects/sht31_common.c
@@ -50,7 +50,7 @@ int init_error; // store exit status of the init function
 /******************************************************************************/
 /*                       Function Prototypes (Private)                        */
 /******************************************************************************/
-unsigned char crc_8( char* data, unsigned char poly, unsigned char init_val );
+unsigned char crc_8( char* data, unsigned int len, unsigned char poly, unsigned char init_val );
 
 
 
@@ -64,21 +64,22 @@ unsigned char crc_8( char* data, unsigned char poly, unsigned char init_val );
  *              It's correctness was verified using the following website:
  *              www.sunshine2k.de/coding/javascript/crc/crc_js.html.
  * Parameters:
- *   data     - array of input data. MUST BE NULL TERMINATED
+ *   data     - array of input data. 
+ *   len      - length of data array
  *   poly     - the generator polynomial
  *   init_val - the initial crc value
- * Date:        02/19/2018
+ * Date:        03/01/2018
 *******************************************************************************/
-unsigned char crc_8( char* data, unsigned char poly, unsigned char init_val )
+unsigned char crc_8( char* data, unsigned int len, unsigned char poly, unsigned char init_val )
 {
   unsigned char rem;
-  int i;
+  int i, j;
 
   rem = init_val;
-  while ( *data != '\0' )
+  for ( i = 0 ; i < len ; i++ )
   {
-    rem ^= *data;
-    for ( i = 0 ; i < 8 ; i++ )
+    rem ^= data[i];
+    for ( j = 0 ; j < 8 ; j++ )
     {
       if ( rem & 0x80 )
       {
@@ -90,8 +91,6 @@ unsigned char crc_8( char* data, unsigned char poly, unsigned char init_val )
         rem <<= 1;
       }
     }
-
-    data++;
   }
 
   return rem;
@@ -104,10 +103,17 @@ unsigned char crc_8( char* data, unsigned char poly, unsigned char init_val )
 /*******************************************************************************
  * Function:    sht31_get_humidity
  * Author:      Matt Mielke
- * Description:   Open the device /dev/i2c-1, which maps to I2C1, in R/W mode 
+ * Description:   Get a single shot measuremnt from the I2C device at 
+ *              /dev/i2c-1. Issuing the signle shot measurement command will 
+ *              cause the device to return temperature and humidity measurements
+ *              along with checksums for each. This function will save the 
+ *              humidity checksum value and recalulate it to ensure the humidity
+ *              bytes were not currputed. The humidity value is then calculated
+ *              and returned. 
  * Parameters:  none
  * Date:        02/19/2018
 *******************************************************************************/
+// TODO: change error codes values that could not be interpreted as temperatures
 float sht31_get_humidity( void )
 {
   // TODO: Abstract the I2C write and I2C read functionality
@@ -142,9 +148,12 @@ float sht31_get_humidity( void )
   hum_crc = r_buff[5];
 
   // recalculate the temperature check sum and compare with the received value
+  // TODO: don't just return. try measurements until checksum is valid.
   r_buff[5] = '\0';
-  if ( crc_8( r_buff + 3, 0x31, 0xFF ) != hum_crc )
+  if ( crc_8( r_buff + 3, 2, 0x31, 0xFF ) != hum_crc )
   {
+    //TODO: remove this
+    //printf( "Buffer: 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X\n", r_buff[0], r_buff[1], r_buff[2], r_buff[3], r_buff[4], hum_crc ); 
     return -5;
   }
 
@@ -159,10 +168,17 @@ float sht31_get_humidity( void )
 /*******************************************************************************
  * Function:    sht31_get_temperature
  * Author:      Matt Mielke
- * Description:   Open the device /dev/i2c-1, which maps to I2C1, in R/W mode 
+ * Description:   Get a single shot measuremnt from the I2C device at 
+ *              /dev/i2c-1. Issuing the signle shot measurement command will 
+ *              cause the device to return temperature and humidity measurements
+ *              along with checksums for each. This function will save the 
+ *              temperature checksum value and recalulate it to ensure the 
+ *              temperature bytes were not currputed. The temperature value is
+ *              then calculated and returned. 
  * Parameters:  none
  * Date:        02/19/2018
 *******************************************************************************/
+// TODO: change error codes values that could not be interpreted as temperatures
 float sht31_get_temperature( void )
 {
   // TODO: Abstract the I2C write and I2C read functionality
@@ -197,9 +213,12 @@ float sht31_get_temperature( void )
   temp_crc = r_buff[2];
 
   // recalculate the temperature check sum and compare with the received value
+  // TODO: don't just return. try measurements until checksum is valid.
   r_buff[2] = '\0';
-  if ( crc_8( r_buff, 0x31, 0xFF ) != temp_crc )
+  if ( crc_8( r_buff, 2, 0x31, 0xFF ) != temp_crc )
   {
+    // TODO: remove this
+    //printf( "Buffer: 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X\n", r_buff[0], r_buff[1], temp_crc,  r_buff[3], r_buff[4], r_buff[5] ); 
     return -5;
   }
 
